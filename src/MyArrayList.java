fd247c1b9fc8a7ec7c2a15a0349b8dbe2e499375
Akzhantai <musabekovaanara17@gmail.com>
2023-04-19 19:23:37 +0600

    MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -108,7 +108,12 @@ public class MyArrayList<E> implements MyList<E>{
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = size-1; i >= 0; i--){
+            if (arr[i].equals(o)){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
