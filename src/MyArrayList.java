f1948b6ee8b4cd3a1446253487c15328a4fed696
Akzhantai <musabekovaanara17@gmail.com>
2023-04-19 18:42:00 +0600

    MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -36,20 +36,50 @@ public class MyArrayList<E> implements MyList<E>{
 
     @Override
     public void add(E item, int index) {
-        if (index < 0 || index > size){
+        if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException();
         }
+        if (size == arr.length) {
+            Object[] newArr = new Object[arr.length * 2];
+            for (int i = 0; i < size; i++) {
+                newArr[i] = arr[i];
+            }
+            for (int i = index; i < size; i++){
+                newArr[i+1] = arr[i];
+            }
+            arr = newArr;
+        }else {
+            for (int i = size -1; i >= index; i++){
+                arr[i+1] = arr[i];
+            }
+        }
+        arr[index] = item;
+        size++;
     }
 
 
     @Override
     public boolean remove(E item) {
+        for (int i = 0; i < size; i++){
+            if (arr[i].equals(item)){
+                remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        E removed = (E) arr[index];
+        for (int i = index; i < size-1; i++){
+            arr[i] = arr[i+1];
+        }
+        size--;
+        return removed;
     }
 
     @Override
