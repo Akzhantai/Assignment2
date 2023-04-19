2354e5e425779d89a426a27bc3fc59953d1d2758
Akzhantai <musabekovaanara17@gmail.com>
2023-04-19 22:10:39 +0600

    MyArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<Integer> myArray = new MyArrayList<Integer>();
-        myArray.add(1);
-        myArray.add(2);
-        myArray.add(9);
-        myArray.add(5);
-        myArray.add(7);
-        myArray.sort();
-        for (int i = 0; i < myArray.size(); i++) {
-            System.out.println(myArray.get(i));
+        MyLinkedList list = new MyLinkedList();
+        for (int i = 0; i < 10; i++){
+            list.add(i);
+        }for (int i = 0; i < 10; i++){
+            System.out.println(list.get(i));
         }
     }
 }
