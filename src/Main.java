adf09ad1e6eab54d0fe530eef307e1fb8968a011
Akzhantai <musabekovaanara17@gmail.com>
2023-04-19 22:26:22 +0600

    MyLinkedList

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         MyLinkedList list = new MyLinkedList();
         for (int i = 0; i < 10; i++){
             list.add(i);
+            list.clear();
         }for (int i = 0; i < 10; i++){
             System.out.println(list.get(i));
         }
